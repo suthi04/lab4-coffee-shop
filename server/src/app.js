4cddc1c00baae9bbafe2b2e730d5646b23ed52c5
suthi04 <suthiph04@gmail.com>
2025-12-11 20:39:31 +0700

    Complete Lab 4 Coffee Shop API

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,48 @@
-let express = require('express');
-const app = express();
-
-app.get('/status', (req, res) => {
-    res.send('Hello Node.js Server!')
-});
-
+let express = require('express')
+let bodyParser = require('body-parser')
+ 
+const app = express()
+ 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+ 
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server')
+})
+ 
 app.get('/hello/:person', function (req, res) {
     console.log('hello - ' + req.params.person)
     res.send('say hello with ' + req.params.person)
-});
-
+})
 // get user by id
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน: ' + req.params.userId)
+})
+ 
+// get all user
+app.get('/users', function (req, res) {
+    res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
+})
+// create user
+app.post('/user/', function (req, res) {
+    res.send('ทำการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
+})
+ 
+// edit user
+app.put('/user/:userId', function (req, res) {
+    res.send('ทำการแก้ไขผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body))
+})
+ 
+// delete user
+app.delete('/user/:userId', function (req, res) {
+    res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body))
+})
+ 
+let port = 8081
+app.listen(port, function () {
+    console.log('server running on ' + port)
+})
+ 
 app.get('/user/:userId', function (req, res) {
     res.send('ดูข้อมูลผู้ใช้งาน')
 })
-// get all user
-app.get('/users', function (req, res) {
-    res.send('เรียกข้อมูลผู้ใช้งาน')
-})
-// get all user
-app.get('/users', function (req, res) {
-    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
-})
-let port = process.env.PORT || 8081;
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
